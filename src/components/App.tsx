fbb14ac77e4db16c710aa77e5329353dee739ae7
Ivan Satsiuk <sativ01.ca@gmail.com>
2021-01-05 23:34:34 +0100

    moving client to typescript with linter

---
+++ src/components/App.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+function App () {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Hello world!</p>
+        <a
+          className="App-link"
+          href="/auth/google"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Login with Google
+        </a>
+      </header>
+    </div>
+  )
+}
+
+export default App
